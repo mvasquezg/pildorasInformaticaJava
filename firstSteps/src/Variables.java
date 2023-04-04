bb7cc49640760ea899fd1d3604a6eb6fd6f3aa07
mvasquezg <ing.mvasquezg@gmail.com>
2023-04-04 17:26:36 -0600

    Programa02.-Creando el programa de variables. Curso Java. Pildoras Informaticas

---
+++ firstSteps/src/Variables.java
@@ -0,0 +1,20 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		//byte age;
+		//short age;
+		
+		int age=35;
+		//age=35;
+		
+		//age=75;
+		System.out.println("Edad:" +age);
+		
+		age=75;
+		System.out.println("Edad:" +age);
+		
+		
+	}
+
+}
