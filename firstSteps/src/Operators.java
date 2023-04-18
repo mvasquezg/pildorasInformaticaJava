9ebdbed1a997d3efe4f4b3d7b8bca13158b29518
mvasquezg <ing.mvasquezg@gmail.com>
2023-04-17 22:50:05 -0600

    Programa04.-Creando el programa de Operadores. Curso Java. Pildoras Informaticas

---
+++ firstSteps/src/Operators.java
@@ -0,0 +1,21 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a=5;
+		
+		int b;
+		
+		b=7;
+		
+		int c=b+a;
+		
+		//c++;
+		
+		//c-=6;
+		
+		System.out.println("Resultado: "+c);
+		
+	}
+
+}
