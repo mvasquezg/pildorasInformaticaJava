e63d56416f110387fb8c6b5c3f5df0b7e5b9e73f
mvasquezg <ing.mvasquezg@gmail.com>
2023-04-17 23:31:08 -0600

    Programa05.-Creando el programa de Constantes. Curso Java. Pildoras Informaticas

--- firstSteps/src/Operators.java
+++ firstSteps/src/Operators.java
@@ -2,19 +2,37 @@
 public class Operators {
 
 	public static void main(String[] args) {
-		int a=5;
+		/*int a=5;
 		
 		int b;
 		
 		b=7;
 		
-		int c=b+a;
+		int c=b+a;*/
 		
 		//c++;
 		
 		//c-=6;
 		
-		System.out.println("Resultado: "+c);
+		//System.out.println("Resultado: "+c);
+		
+		//Declaracion de constante
+		/*final int c=5;
+		
+		//c=8;
+		
+		System.out.println("Resultado: "+c);*/
+		
+		
+		//Conversion a pulgadas
+		
+		final double aPulgadas=2.54;
+		
+		double cm=6;
+		
+		double result=cm/aPulgadas;
+		
+		System.out.println("En "+cm+" hay "+result+" pulgadas");
 		
 	}
 
