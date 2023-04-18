5d4c4f01339f6ef0f22b70ea62e8f0ed6034d6a6
mvasquezg <ing.mvasquezg@gmail.com>
2023-04-18 13:55:37 -0600

    Programa06.-Haciendo uso del casteo, aplicando la clase Math. Curso Java. Pildoras Informaticas

--- firstSteps/src/ClassMath.java
+++ firstSteps/src/ClassMath.java
@@ -2,9 +2,20 @@
 public class ClassMath {
 
 	public static void main(String[] args) {
-		double root=Math.sqrt(9);
+		//double root=Math.sqrt(9);
 		
-		System.out.println("La raiz de 9 es: "+root);
+		//System.out.println("La raiz de 9 es: "+root);
+	
+		//double num=5.85;
+		
+		//int result=(int) Math.round(num);
+		
+		double base=5;
+		double exponent=3;
+		
+		int result=(int) Math.pow(base, exponent);
+		
+		System.out.println("Valor: "+result);
 	}
 
 }
