31a62627ba24a22c8caf786503056d57d6d69a70
mvasquezg <ing.mvasquezg@gmail.com>
2023-04-20 11:50:24 -0600

    Programa07.-Usando metodo de la clase String, substring, equals, equalsIgnoreCase. Curso Java. Pildoras Informaticas

--- firstSteps/src/UseClassString.java
+++ firstSteps/src/UseClassString.java
@@ -2,7 +2,7 @@
 public class UseClassString {
 
 	public static void main(String[] args) {
-		String name="Miguel";
+		/*String name="Miguel";
 		
 		System.out.println("Nombre: "+name+"\nEl nombre tiene: "+name.length()+" letras");
 		
@@ -11,7 +11,21 @@ public class UseClassString {
 		int lastLetter=name.length();
 		
 		System.out.println("La ultima letra de "+name+" es la letra "+name.charAt(lastLetter-1));
-
+		*/
+		
+		/*String phrase="Hoy es un estupendo dia para aprender a programar en Java.";
+		
+		String sumaryPhrase=phrase.substring(0, 29)+"irnos a la playa y olvidarnos de todo... y "+
+		phrase.substring(29, 57);
+		
+		System.out.println("Esto es lo extraido: "+sumaryPhrase );*/
+		
+		String student1, student2;
+		
+		student1="David";
+		student2="david";
+		
+		System.out.println(student1.equalsIgnoreCase(student2));
 				
 	}
 
