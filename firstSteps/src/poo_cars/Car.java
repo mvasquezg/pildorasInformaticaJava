d08a0ce14db9fe8f3c1c2fe2c25739fdc3784ef7
mvasquezg <ing.mvasquezg@gmail.com>
2023-06-07 19:25:32 -0600

    Programa24.-Construyendo el primer programa orientado a objetos con dos clases. Pildoras Informaticas

---
+++ firstSteps/src/poo_cars/Car.java
@@ -0,0 +1,24 @@
+package poo_cars;
+
+public class Car {
+	
+	int tire;
+	int length;
+	int width;	
+	int carEngine;
+	int weight;
+	
+	public Car() {
+		this.tire=4;
+		this.length=2000;
+		this.width=300;
+		this.carEngine=1600;
+		this.weight=500;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ firstSteps/src/poo_cars/CarsApp.java
@@ -0,0 +1,15 @@
+package poo_cars;
+
+public class CarsApp {
+
+	public static void main(String[] args) {
+		
+		Car car1=new Car(); //Instaciar clase. Ejemplar de Clase
+		
+		
+		System.out.println("El coche car1 tiene "+car1.tire+" llantas");
+		
+
+	}
+
+}
