57ceb5b7f3c9d1e446694a7456d1e94d68bbba03
mvasquezg <ing.mvasquezg@gmail.com>
2023-06-08 17:44:21 -0600

    Programa24.-Haciendo uso de los modificadores de acceso private y de los metodos setters y getters. Pildoras Informaticas

--- firstSteps/src/poo_cars/Car.java
+++ firstSteps/src/poo_cars/Car.java
@@ -2,21 +2,38 @@ package poo_cars;
 
 public class Car {
 	
-	int tire;
-	int length;
-	int width;	
-	int carEngine;
-	int weight;
+	private int tire;
+	private int length;
+	private int width;	
+	private int carEngine;
+	private int platformWeight;
+	
+	private String color;
+	private int totalWeight;
+	private boolean airConditioning;
+	private boolean leatherSeats;
+	
 	
 	public Car() {
 		this.tire=4;
 		this.length=2000;
 		this.width=300;
 		this.carEngine=1600;
-		this.weight=500;
+		this.platformWeight=500;
+	}
+	
+	public String getLength() {
+		return "El largo del coche es: "+this.length+" cm";
 	}
 	
 	
+	public void setColor() {
+		this.color="Azul";
+	}
+	
+	public String getColor() {
+		return "El color del coche es: "+this.color;
+	}
 	
 	
 	
--- firstSteps/src/poo_cars/CarsApp.java
+++ firstSteps/src/poo_cars/CarsApp.java
@@ -6,9 +6,9 @@ public class CarsApp {
 		
 		Car car1=new Car(); //Instaciar clase. Ejemplar de Clase
 		
-		
-		System.out.println("El coche car1 tiene "+car1.tire+" llantas");
-		
+		car1.setColor();
+		//System.out.println("El coche car1 tiene "+car1.tire+" llantas");
+		System.out.println(car1.getLength()+"\n"+car1.getColor());
 
 	}
 
