dce9d4dfd8186628d096fabcac1f3b9b30574ffe
mvasquezg <ing.mvasquezg@gmail.com>
2023-06-26 11:20:28 -0600

    Programa24.-Recibiendo parametros con la clase JOptionPane. Pildoras Informaticas

--- firstSteps/src/poo_cars/CarsApp.java
+++ firstSteps/src/poo_cars/CarsApp.java
@@ -1,20 +1,25 @@
 package poo_cars;
 
+import javax.swing.JOptionPane;
+
 public class CarsApp {
 
 	public static void main(String[] args) {
 		
 		Car car1=new Car(); //Instaciar clase. Ejemplar de Clase
 		
-		car1.setColor("Marron");
+		//car1.setColor("Marron");
+		car1.setColor(JOptionPane.showInputDialog("Ingresa el color del vehiculo: "));
 		//System.out.println("El coche car1 tiene "+car1.tire+" llantas");
 		System.out.println(car1.getGeneralData()+"\n"+car1.getLength()+"\n"+car1.getColor());
 		
-		car1.setLeatherSeats("SI");
-		
+		//car1.setLeatherSeats("SI");
+		car1.setLeatherSeats(JOptionPane.showInputDialog("El vehiculo tiene asientos de cuero si/no: "));
 		System.out.println(car1.getLeatherSeats());
 		
-		car1.setAirConditioning("si");
+		//car1.setAirConditioning("si");
+		car1.setAirConditioning(JOptionPane.showInputDialog("El vehiculo tiene aire acondicionado si/no: "));
+		
 		
 		System.out.println(car1.getAirConditioning());
 		
