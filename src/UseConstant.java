4ccbeeccdd20d88c591a417a27a51ebae23729ec
mavg <ing.mvasquezg@gmail.com>
2025-03-18 08:06:46 -0600

    Ejcutando el porgrama del video 09 y empezar en el 10

--- src/UseConstant.java
+++ src/UseConstant.java
@@ -4,10 +4,18 @@ public class UseConstant {
 		
 		/*final int example;
 		
-		example=9;*/
+		example=9;
 		
-		final int example=9;
+		final int example=9;*/
 		
+		final double inches=2.54;
+		
+		int cm=7;
+		
+		double result=inches*cm;
+		
+		System.out.println("En "+cm+" cm hay "+result+" pugadas");
+
 		
 	}
 
