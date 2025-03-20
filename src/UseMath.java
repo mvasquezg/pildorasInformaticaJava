e3c068cbbf1b4e2df4dbbea73c929a055af0c3c4
mavg <ing.mvasquezg@gmail.com>
2025-03-19 20:58:09 -0600

    Uso de la clase Math

---
+++ src/UseMath.java
@@ -0,0 +1,43 @@
+import java.math.BigInteger;
+
+public class UseMath {
+
+	public static void main(String[] args) {
+		//int root=(int) Math.sqrt(9);
+		
+		/*double root=Math.sqrt(9);
+		System.out.println("La raiz de 9 es: "+root);*/
+		
+		/*int power=(int) Math.pow(25, 1250);
+		System.out.println("La potencia de de 25 es: "+power);*/
+		
+		/*BigInteger base=BigInteger.valueOf(25);
+		
+		int exponente=321;
+		
+		BigInteger power=base.pow(exponente);
+		
+		System.out.println(power);*/
+		
+		/*double random=Math.random()*100;
+		
+		System.out.println(random);*/
+		
+		/*int random=(int)(Math.random()*100);
+		
+		System.out.println(random);*/
+		
+		/*int random=Math.round((float)(Math.random()*100));
+		
+		System.out.println(random);*/
+		
+		/*int absolut=Math.abs(-45);
+		
+		System.out.println(absolut);*/
+		
+		int max=Math.max(3, 10);
+		
+		System.out.println(max);
+	}
+
+}
