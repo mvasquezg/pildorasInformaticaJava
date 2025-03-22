7f681aa5bf7cb8a80fb469264549ab91b02c315d
mavg <ing.mvasquezg@gmail.com>
2025-03-22 07:12:14 -0600

    Uso de la clase String

---
+++ src/UseString.java
@@ -0,0 +1,37 @@
+
+public class UseString {
+
+	public static void main(String[] args) {
+		
+		/*String name="Ana";
+	
+		
+		System.out.println("El nomnbre: "+name+" tiene "+name.length()+" letras");
+		
+		System.out.println("la primera letra del nombre es: "+name.charAt(0));
+		
+		System.out.println("la ultima letra del nombre es: "+name.charAt(name.length()-1));
+		*/
+		
+		/*String phrase="Hoy es un estupendo dia para programar en Java";
+		
+		String subString=phrase.substring(4, 6);
+		
+		System.out.println(subString);
+		
+		String subString=phrase.substring(4);
+		
+		System.out.println(subString);*/
+		
+		/*String student1="David";
+		String student2="david";
+		
+		//System.out.println(student1.equals(student2));
+		System.out.println(student1.equalsIgnoreCase(student2));*/
+		
+		String message="hola";
+		
+		
+	}
+
+}
